4fe6b28082b2f728139b39e55b9b685bd6e45901
Betül Çalık <betulcalik82@icloud.com>
2023-08-26 14:37:33 +0300

    Initial Commit

---
+++ ios-unity-integration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-unity-integration
+//
+//  Created by Betül Çalık on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
